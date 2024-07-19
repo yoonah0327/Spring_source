058e5557e74667afec33e14bb642d267aa190bbc
yoonah0327 <yoonah6719@gmail.com>
2024-07-19 14:19:49 +0900

    빠뜨렸던 파일들 추가업로드.

---
+++ sprweb16jpa_mem_crud/src/main/java/pack/Sprweb16jpaMemCrudApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb16jpaMemCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb16jpaMemCrudApplication.class, args);
+	}
+
+}
